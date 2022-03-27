331ae8f452e89db4a7da8dd8680df876cfb2710b
IvessJohn <vanek2v@gmail.com>
2022-03-27 13:03:01 +0300

    Add favicon

--- expenses/expenses/urls.py
+++ expenses/expenses/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tracker.urls'))
+    path('', include('tracker.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
 ]
